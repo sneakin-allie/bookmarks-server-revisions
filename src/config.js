577ea739a61ce734106a24bdf077e11bbe7891ea
Allison Schulman <allison.d.schulman@gmail.com>
2021-02-24 13:18:36 -0500

    Commit with revisions

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    API_TOKEN: process.env.API_TOKEN || '03948556-c348-405e-aa80-aacf47be7a78',
+}
